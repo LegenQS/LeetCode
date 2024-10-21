de00b60da2c9bcd9b8790dfc07469ccfa526cf3e
LegenQS <qw2360@columbia.edu>
2024-10-20 23:48:21 -0400

    Oct 20

---
+++ src/com.qs.leetcode/code/array/BinarySearch.java
@@ -0,0 +1,110 @@
+package code.array;
+
+/**
+ * @author LegenQS
+ * @date 10/20/24 11:09 PM
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,1,2,3,4};
+        System.out.println(search(nums, 1));
+    }
+    /**
+     * There is an integer array nums sorted in ascending order (with distinct values).
+     *
+     * Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k
+     * (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1],
+     * ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become
+     * [4,5,6,7,0,1,2].
+     *
+     * Given the array nums after the possible rotation and an integer target, return the index of target if it is in
+     * nums, or -1 if it is not in nums.
+     *
+     * You must write an algorithm with O(log n) runtime complexity.
+     */
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            System.out.println(left + " " + right);
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                if (isMax(nums[mid], nums[left], nums[right])) {
+                    if (nums[left] > target) {
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid;
+                    }
+                }
+                else if (isMin(nums[mid], nums[left], nums[right])) {
+                    right = mid;
+                }
+                else {
+                    if (nums[left] > target) {
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid;
+                    }
+                }
+            }
+            else {
+                if (isMin(nums[mid], nums[left], nums[right])) {
+                    if (nums[right] >= target) {
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid;
+                    }
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isMax(int a, int b, int c) {
+        return a > b && a > c;
+    }
+
+    private static boolean isMin(int a, int b, int c) {
+        return a < b && a < c;
+    }
+
+    /**
+     * Simplified version, comparing the value only with the boundary and the mid.
+     */
+    public static int searchII(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            System.out.println(left + " " + right);
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/com.qs.leetcode/code/array/Swap.java
@@ -0,0 +1,66 @@
+package code.array;
+
+/**
+ * Class of swap algorithm used in arrays.
+ *
+ * @author LegenQS
+ * @date 10/20/24 10:42 PM
+ */
+public class Swap {
+    /**
+     * LC-26:
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique
+     * element appears only once. The relative order of the elements should be kept the same. Then return the number of
+     * unique elements in nums. Consider the number of unique elements of nums to be k, to get accepted, you need to do the
+     * following things:
+     *
+     * Change the array nums such that the first k elements of nums contain the unique elements in the order they were
+     * present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+     *
+     * Space complexity: O(1)
+     * Time complexity: O(n)
+     */
+    public int removeDuplicatesI(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int index = 0;
+        for (int num : nums) {
+            if (num == nums[index]) {
+                nums[++index] = num;
+            }
+        }
+        return index + 1;
+    }
+
+    /**
+     * LC-80:
+     * Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique
+     * element appears at most twice. The relative order of the elements should be kept the same. Since it is impossible
+     * to change the length of the array in some languages, you must instead have the result be placed in the first part
+     * of the array nums. More formally, if there are k elements after removing the duplicates, then the first k
+     * elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+     *
+     * Return k after placing the final result in the first k slots of nums. Do not allocate extra space for another
+     * array. You must do this by modifying the input array in-place with O(1) extra memory.
+     */
+    public int removeDuplicatesII(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        if (nums.length < 3) {
+            return nums.length;
+        }
+        // initial start position
+        int index = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
